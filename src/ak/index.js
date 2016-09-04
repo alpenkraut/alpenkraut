f0b3afe8271e42780bf3d36d1f74147091753dde
Te Standby <testandby@gmail.com>
2016-09-04 20:09:54 +0200

    Configin window

--- src/ak/index.js
+++ src/ak/index.js
@@ -1,8 +1,12 @@
 import {app, BrowserWindow as Win} from "electron"
 
 app.on("ready", ()=> {
-
-    const win = new Win()
+    const win = new Win({
+        backgroundColor: "#4c9d9d",
+        icon: "icon.png",
+        height: 576,
+        width: 1024,
+    })
     const contentPath = require.resolve("../win/index.html")
     win.loadURL("file://" + contentPath)
     win.webContents.openDevTools()
